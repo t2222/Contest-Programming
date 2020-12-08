2666e0c50270898d0a69e795fae3e31935218883
t2222 <mtousif2222@gmail.com>
2020-12-08 17:08:23 +0600

    8-12-20

---
+++ 1.IO.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+    string s;
+    int a, b, c, ans;
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+//    cin >> a >> b >> c;
+//    cin >> s;
+    scanf("%d %d %d", &a, &b, &c);
+    char str[50];
+    scanf("%s", str);
+    ans = a ? b : c; // to simplify: if (a) ans = b; else ans = c;
+//    cout << ans << "\n";
+//    cout << s << "\n";
+    printf("%d\n", ans);
+    printf("%s\n", str);
+    return 0;
+}
+
